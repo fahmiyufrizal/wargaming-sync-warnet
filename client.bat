477268fffa9f9568475afe7bb0732c5cb9ab7349
Fahmi Yufrizal Yusuf <60032856+fahmiyufrizal@users.noreply.github.com>
2021-03-30 21:30:58 +0700

    client-side-script

---
+++ client.bat
@@ -0,0 +1,6 @@
+@echo off
+%SystemRoot%\System32\reg.exe import _fahmiyufrizal/import_reg.reg
+mklink /J "%ProgramData%\Wargaming.net" "%~dp0\_fahmiyufrizal\ProgramData\Wargaming.net"
+start wgc.exe
+cd _fahmiyufrizal
+call cleanup.bat
