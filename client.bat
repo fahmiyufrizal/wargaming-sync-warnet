12e4a4f3e1f758603f82f64f221d54005dac84a1
Fahmi Yufrizal Yusuf <60032856+fahmiyufrizal@users.noreply.github.com>
2021-04-01 23:01:13 +0700

    Update remove existing config

--- client.bat
+++ client.bat
@@ -1,4 +1,5 @@
 @echo off
+RD /S /Q "%ProgramData%\Wargaming.net"
 %SystemRoot%\System32\reg.exe import _fahmiyufrizal/import_reg.reg
 mklink /J "%ProgramData%\Wargaming.net" "%~dp0\_fahmiyufrizal\ProgramData\Wargaming.net"
 start wgc.exe
